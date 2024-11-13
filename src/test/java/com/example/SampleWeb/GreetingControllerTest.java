6ad6da71995f3ee65373948d9209057f478fa750
Maphineth <maphinethung@gmail.com>
2024-11-13 21:27:10 +0100

    Create GreetingControllerTest.java

---
+++ src/test/java/com/example/SampleWeb/GreetingControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.SimpleWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingControllerTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
